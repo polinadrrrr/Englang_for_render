6531f19475d721998686e2aa21ad9259de627dd8
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2024-05-26 16:50:55 +0700

    Delete theory directory

--- theory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Theme, Lesson, Comment, Tag, Textbook
-
-# Register your models here.
-admin.site.register(Theme)
-admin.site.register(Lesson)
-admin.site.register(Comment)
-admin.site.register(Tag)
-admin.site.register(Textbook)
