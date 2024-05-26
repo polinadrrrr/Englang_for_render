1a1acf0c7355b5f1154e836d522638306850627e
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2024-05-26 16:49:16 +0700

    Delete exercises directory

--- exercises/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Text, Word, Topic, Exercise
-
-# Register your models here.
-admin.site.register(Text)
-admin.site.register(Word)
-admin.site.register(Topic)
-admin.site.register(Exercise)
