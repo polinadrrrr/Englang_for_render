829b51869c4e02e68f930adf749991adf60eee8d
polinadrrrr <99727608+polinadrrrr@users.noreply.github.com>
2024-06-16 17:31:32 +0700

    Update settings.py

--- main/main/settings.py
+++ main/main/settings.py
@@ -160,7 +160,18 @@ STATICFILES_FINDERS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "englang.4.everyone@gmail.com"
+EMAIL_HOST_PASSWORD = "lcisotzdyyfsjfxn"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
